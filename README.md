a188d59b2198cd62bdd8e98bf794993a74dda793
BeamlakF <beamlakfekadu562@gmail.com>
2025-11-14 18:36:46 +0300

    Use the TypedResults API

---
+++ README.md
@@ -0,0 +1,5 @@
+# Beamlak Fekadu Hailemariam
+
+# UGR/8928/15
+
+# EAD-LAB-1
--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,8 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
-
-
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument(config =>
@@ -26,19 +25,21 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-app.MapGet("/todoitems", async (TodoDb db) =>
+var todoItems = app.MapGroup("/todoitems");
+
+todoItems.MapGet("/", async (TodoDb db) =>
     await db.Todos.ToListAsync());
 
-app.MapGet("/todoitems/complete", async (TodoDb db) =>
+todoItems.MapGet("/complete", async (TodoDb db) =>
     await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
-app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+todoItems.MapGet("/{id}", async (int id, TodoDb db) =>
     await db.Todos.FindAsync(id)
         is Todo todo
             ? Results.Ok(todo)
             : Results.NotFound());
 
-app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+todoItems.MapPost("/", async (Todo todo, TodoDb db) =>
 {
     db.Todos.Add(todo);
     await db.SaveChangesAsync();
@@ -46,7 +47,7 @@ app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
     return Results.Created($"/todoitems/{todo.Id}", todo);
 });
 
-app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+todoItems.MapPut("/{id}", async (int id, Todo inputTodo, TodoDb db) =>
 {
     var todo = await db.Todos.FindAsync(id);
 
@@ -60,7 +61,7 @@ app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
     return Results.NoContent();
 });
 
-app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+todoItems.MapDelete("/{id}", async (int id, TodoDb db) =>
 {
     if (await db.Todos.FindAsync(id) is Todo todo)
     {
@@ -73,3 +74,81 @@ app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
 });
 
 app.Run();
+
+
+// using Microsoft.EntityFrameworkCore;
+// var builder = WebApplication.CreateBuilder(args);
+// builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+
+
+
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddOpenApiDocument(config =>
+// {
+//     config.DocumentName = "TodoAPI";
+//     config.Title = "TodoAPI v1";
+//     config.Version = "v1";
+// });
+
+// var app = builder.Build();
+
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseOpenApi();
+//     app.UseSwaggerUi(config =>
+//     {
+//         config.DocumentTitle = "TodoAPI";
+//         config.Path = "/swagger";
+//         config.DocumentPath = "/swagger/{documentName}/swagger.json";
+//         config.DocExpansion = "list";
+//     });
+// }
+
+// app.MapGet("/todoitems", async (TodoDb db) =>
+//     await db.Todos.ToListAsync());
+
+// app.MapGet("/todoitems/complete", async (TodoDb db) =>
+//     await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+// app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+//     await db.Todos.FindAsync(id)
+//         is Todo todo
+//             ? Results.Ok(todo)
+//             : Results.NotFound());
+
+// app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+// {
+//     db.Todos.Add(todo);
+//     await db.SaveChangesAsync();
+
+//     return Results.Created($"/todoitems/{todo.Id}", todo);
+// });
+
+// app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) =>
+// {
+//     var todo = await db.Todos.FindAsync(id);
+
+//     if (todo is null) return Results.NotFound();
+
+//     todo.Name = inputTodo.Name;
+//     todo.IsComplete = inputTodo.IsComplete;
+
+//     await db.SaveChangesAsync();
+
+//     return Results.NoContent();
+// });
+
+// app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
+// {
+//     if (await db.Todos.FindAsync(id) is Todo todo)
+//     {
+//         db.Todos.Remove(todo);
+//         await db.SaveChangesAsync();
+//         return Results.NoContent();
+//     }
+
+//     return Results.NotFound();
+// });
+
+// app.Run();
+
